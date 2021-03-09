4613778e4ec95469caff016026ea2167a2e96149
Ming <minghaochen912@gmail.com>
2021-03-08 22:46:45 -0800

    changed button location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,22 +6,48 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faUserFriends } from '@fortawesome/free-solid-svg-icons';
+
 import Main from './components/main.tsx';
 
 function App() {
   // ALL_SOURCES: 1, ADD_SOURCE: 2, EDIT_SOURCE: 3
-  let [pageToLoad, setPageToLoad] = useState(0);
+  let [pageToLoad, setPageToLoad] = useState(1);  // Automatically start with all sources loaded
 
   // Gets rid of previous screen and renders the "GetAllSources" screen
-  function renderAllSources() {
-    setPageToLoad(1);
-    console.log("Displaying new page: " + pageToLoad);
+  function renderPage() {
+    switch (pageToLoad) {
+      case 1:
+        setPageToLoad(2);
+        break;
+      case 2:
+        setPageToLoad(1);
+        break;
+      case 3: // Do nothing
+        break;
+    }
   }
-  
+
+  // Get correct icon type
+  function getIconType() {
+    switch (pageToLoad) {
+      case 1:
+        return faPlus;
+      case 2:
+        return faUserFriends;
+      case 3: // Do nothing
+        break;
+    }
+  }
+
   return (<Container style={{alignContent: "center"}}>
-    <Row className="justify-content-md-center my-3">
-        <Button onClick={renderAllSources}>Get All Sources</Button>
-    </Row>
+    <header className={"text-center mt-2"}>
+      <h1>Sources</h1>
+    </header>
+    <Button className={"float pointer-hover"} onClick={renderPage}>
+      <FontAwesomeIcon icon={getIconType()} size={"2x"} className={"centered-item"} />
+    </Button>
     <Main toLoad={pageToLoad}/>
   </Container>);
 }
--- frontend/src/components/createSource.tsx
+++ frontend/src/components/createSource.tsx
@@ -25,7 +25,7 @@ export default function CreateSource(props: any) {
     }
 
     return (
-        <div>
+        <div style={{"marginTop": "100px"}}>
             <label>Name: </label>  <input onChange={(e) => setName(e.target.value)} />
             <label>Organization: </label><input onChange={(e) => setOrganization(e.target.value)} />
             <label>Phone: </label> <input onChange={(e) => setPhone(e.target.value)} />
